514269238429a2f9ccb6457fdd491122ac75ed2d
Joshua Berry <joshua.berry92@gmail.com>
2016-10-10 14:34:52 +0100

    Testing routes

--- PokerGameFE/js/index.jsx
+++ PokerGameFE/js/index.jsx
@@ -1,12 +1,17 @@
+import {Router, Route, IndexRoute, hashHistory} from "react-router";
+
 var NavBar = require("./components/navBar.jsx")
 var LogIn = require("./components/logIn.jsx")
 var Game = require("./components/game.jsx")
 
 ReactDOM.render(
-  <div>
-    <NavBar />
-    <LogIn />
-    <Game />
-  </div>,
-  document.getElementById('container')
-);
+
+  <Router history={hashHistory}>
+    <div>
+
+      <NavBar/>
+      <LogIn/>
+      <Game/>
+    </div>,
+  </Router>
+document.getElementById('container'));
