b646819fe5f9455e6c0d13d41933fd612f3f7c8e
Adam Flanagan <adam.flanagan@skybettingandgaming.com>
2016-10-10 12:42:40 +0100

    gitted for berry

--- PokerGameFE/js/index.jsx
+++ PokerGameFE/js/index.jsx
@@ -1,10 +1,12 @@
 var NavBar = require("./components/navBar.jsx")
 var LogIn = require("./components/logIn.jsx")
+var Game = require("./components/game.jsx")
 
 ReactDOM.render(
   <div>
     <NavBar />
     <LogIn />
+    <Game />
   </div>,
   document.getElementById('container')
 );
